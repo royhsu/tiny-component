05b9a5e859636e48f6591e5b93b2ff0cdf286e6a
Roy Hsu <roy.hsu@tinyworld.cc>
2018-05-25 17:17:49 +0800

    add component content mode.

---
+++ Sources/ComponentContentMode.swift
@@ -0,0 +1,47 @@
+//
+//  ComponentContentMode.swift
+//  TinyComponent
+//
+//  Created by Roy Hsu on 2018/5/25.
+//  Copyright © 2018 TinyWorld. All rights reserved.
+//
+
+// MARK: - ComponentContentMode
+
+public enum ComponentContentMode {
+    
+    case fixed(size: CGSize)
+    
+    case automatic(estimatedSize: CGSize)
+    
+}
+
+// MARK: - Equatable
+
+extension ComponentContentMode: Equatable {
+    
+    public static func == (
+        lhs: ComponentContentMode,
+        rhs: ComponentContentMode
+    )
+    -> Bool {
+        
+        switch (lhs, rhs) {
+            
+        case let (
+            .fixed(lhsSize),
+            .fixed(rhsSize)
+        ): return lhsSize == rhsSize
+            
+        case let (
+            .automatic(lhsSize),
+            .automatic(rhsSize)
+        ): return lhsSize == rhsSize
+            
+        default: return false
+            
+        }
+            
+    }
+    
+}
---
+++ Tests/ComponentContentModeTests.swift
@@ -0,0 +1,70 @@
+//
+//  ComponentContentModeTests.swift
+//  ComponentContentModeTests
+//
+//  Created by Roy Hsu on 2018/5/25.
+//  Copyright © 2018 TinyWorld. All rights reserved.
+//
+
+// MARK: - ComponentContentModeTests
+
+import XCTest
+
+@testable import TinyComponent
+
+internal final class ComponentContentModeTests: XCTestCase {
+    
+    internal final func testEqualModes() {
+        
+        XCTAssertEqual(
+            ComponentContentMode.fixed(
+                size: CGSize(
+                    width: 50.0,
+                    height: 50.0
+                )
+            ),
+            ComponentContentMode.fixed(
+                size: CGSize(
+                    width: 50.0,
+                    height: 50.0
+                )
+            )
+        )
+        
+        XCTAssertEqual(
+            ComponentContentMode.automatic(
+                estimatedSize: CGSize(
+                    width: 50.0,
+                    height: 50.0
+                )
+            ),
+            ComponentContentMode.automatic(
+                estimatedSize: CGSize(
+                    width: 50.0,
+                    height: 50.0
+                )
+            )
+        )
+        
+    }
+    
+    internal final func testUnequalModes() {
+        
+        XCTAssertNotEqual(
+            ComponentContentMode.fixed(
+                size: CGSize(
+                    width: 50.0,
+                    height: 50.0
+                )
+            ),
+            ComponentContentMode.automatic(
+                estimatedSize: CGSize(
+                    width: 50.0,
+                    height: 50.0
+                )
+            )
+        )
+        
+    }
+    
+}
