d69ed2ed3c696732c0bdf4d25c26e8662da6bba2
Roy Hsu <roy.hsu@tinyworld.cc>
2018-05-26 19:02:26 +0800

    add default implementation of collection component.

---
+++ Sources/UIKit/UICollectionViewBridge.swift
@@ -0,0 +1,118 @@
+//
+//  UICollectionViewBridge.swift
+//  TinyKit
+//
+//  Created by Roy Hsu on 18/03/2018.
+//  Copyright © 2018 TinyWorld. All rights reserved.
+//
+
+// MARK: - UICollectionViewBridge
+
+public final class UICollectionViewBridge: NSObject {
+
+    private final unowned let collectionView: UICollectionView
+    
+    private final let cellIdentifier = "UICollectionViewCell"
+
+    public init(collectionView: UICollectionView) {
+
+        self.numberOfSections = 0
+        
+        self.numberOfItemsProvider = { _ in 0 }
+        
+        self.sizeForItemProvider = { _, _ in .zero }
+        
+        self.collectionView = collectionView
+
+        super.init()
+
+        setUpCollectionView(collectionView)
+
+    }
+    
+    // MARK: Set Up
+
+    fileprivate final func setUpCollectionView(_ collectionView: UICollectionView) {
+
+        collectionView.register(
+            UICollectionViewCell.self,
+            forCellWithReuseIdentifier: cellIdentifier
+        )
+
+        collectionView.dataSource = self
+
+        collectionView.delegate = self
+
+    }
+
+    public final var numberOfSections: Int
+
+    public typealias NumberOfItemsProvider = (_ section: Int) -> Int
+
+    public final var numberOfItemsProvider: NumberOfItemsProvider
+
+    public typealias ConfigureCellHandler = (UICollectionViewCell, IndexPath) -> Void
+
+    public final var configureCellHandler: ConfigureCellHandler?
+
+    public typealias SizeForItemProvider = (UICollectionViewLayout, IndexPath) -> CGSize
+
+    public final var sizeForItemProvider: SizeForItemProvider
+
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension UICollectionViewBridge: UICollectionViewDataSource {
+
+    public final func numberOfSections(in collectionView: UICollectionView) -> Int { return numberOfSections }
+
+    public final func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    )
+    -> Int { return numberOfItemsProvider(section) }
+
+    public final func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    )
+    -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: cellIdentifier,
+            for: indexPath
+        )
+
+        cell.contentView.subviews.forEach { $0.removeFromSuperview() }
+
+        configureCellHandler?(
+            cell,
+            indexPath
+        )
+
+        return cell
+
+    }
+
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension UICollectionViewBridge: UICollectionViewDelegateFlowLayout {
+
+    public final func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    )
+    -> CGSize {
+
+        return sizeForItemProvider(
+            collectionViewLayout,
+            indexPath
+        )
+
+    }
+
+}
--- Tests/UIKit/UICollectionComponentTests.swift
+++ Tests/UIKit/UICollectionComponentTests.swift
@@ -1,74 +1,70 @@
 //
-//  UIListComponentTests.swift
+//  UICollectionComponentTests.swift
 //  TinyComponentTests
 //
-//  Created by Roy Hsu on 28/01/2018.
+//  Created by Roy Hsu on 02/03/2018.
 //  Copyright © 2018 TinyWorld. All rights reserved.
 //
 
-// MARK: - UIListComponentTests
+// MARK: - UICollectionComponentTests
 
 import XCTest
 
 @testable import TinyComponent
 
-internal final class UIListComponentTests: XCTestCase {
+internal final class UICollectionComponentTests: XCTestCase {
 
     internal final func testInitialize() {
-
-        let listComponent = UIListComponent()
-
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        let collectionComponent = UICollectionComponent(layout: layout)
+        
         XCTAssertEqual(
-            listComponent.contentMode,
+            collectionComponent.contentMode,
             .automatic(estimatedSize: .zero)
         )
-
+        
         XCTAssertEqual(
-            listComponent.tableView,
-            listComponent.view
+            collectionComponent.collectionView,
+            collectionComponent.view
         )
-
+        
+        XCTAssert(collectionComponent.collectionViewLayout === layout)
+        
         XCTAssertEqual(
-            listComponent.tableView.style,
-            .plain
-        )
-
-        XCTAssertEqual(
-            listComponent.view.frame,
+            collectionComponent.view.frame,
             .zero
         )
-
+        
         XCTAssertEqual(
-            listComponent.view.backgroundColor,
+            collectionComponent.view.backgroundColor,
             .clear
         )
-
+        
         XCTAssertEqual(
-            listComponent.numberOfSections,
+            collectionComponent.numberOfSections,
             0
         )
-
-        XCTAssertNil(listComponent.headerComponent)
-
-        XCTAssertNil(listComponent.footerComponent)
-
+        
     }
-
+    
     internal final func testRenderWithContentModeFixed() {
-
-        let listComponent = UIListComponent(
+        
+        let collectionComponent = UICollectionComponent(
             contentMode: .fixed(
                 size: CGSize(
                     width: 500.0,
                     height: 500.0
                 )
-            )
+            ),
+            layout: UICollectionViewFlowLayout()
         )
-
-        listComponent.render()
-
+        
+        collectionComponent.render()
+        
         XCTAssertEqual(
-            listComponent.view.frame.size,
+            collectionComponent.view.frame.size,
             CGSize(
                 width: 500.0,
                 height: 500.0
@@ -76,13 +72,13 @@ internal final class UIListComponentTests: XCTestCase {
         )
 
     }
-
+    
     internal final func testRenderWithContentModeAutomatic() {
-
+       
         let redView = UIView()
-
+        
         redView.backgroundColor = .red
-
+        
         let redComponent = UIItemComponent(
             contentMode: .fixed(
                 size: CGSize(
@@ -92,13 +88,13 @@ internal final class UIListComponentTests: XCTestCase {
             ),
             itemView: redView
         )
-
+        
         let label = UILabel()
-
+        
         label.numberOfLines = 0
-
+        
         label.text = "Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper."
-
+        
         let labelComponent = UIItemComponent(
             contentMode: .automatic(
                 estimatedSize: CGSize(
@@ -108,56 +104,57 @@ internal final class UIListComponentTests: XCTestCase {
             ),
             itemView: label
         )
-
-        let listComponent = UIListComponent(
+        
+        let collectionComponent = UICollectionComponent(
             contentMode: .automatic(
                 estimatedSize: CGSize(
                     width: 500.0,
                     height: 50.0
                 )
-            )
+            ),
+            layout: UICollectionViewFlowLayout()
         )
-
-        listComponent.setItemComponents(
+        
+        collectionComponent.setItemComponents(
             [
                 redComponent,
                 labelComponent
             ]
         )
-
-        listComponent.render()
-
+        
+        collectionComponent.render()
+        
         let expectedLabelSize = label.sizeThatFits(
             CGSize(
-                width: 500.0,
+                width: 50.0,
                 height: 50.0
             )
         )
-
+        
         XCTAssertEqual(
-            listComponent.view.frame.size.width,
+            collectionComponent.view.frame.size.width,
             500.0,
             accuracy: .greatestFiniteMagnitude
         )
-
+        
         XCTAssertEqual(
-            listComponent.view.frame.size.height,
-            100 + expectedLabelSize.height,
+            collectionComponent.view.frame.size.height,
+            100.0 + expectedLabelSize.height,
             accuracy: .greatestFiniteMagnitude
         )
-
-        let firstItemComponent = listComponent.itemComponent(
+        
+        let firstItemComponent = collectionComponent.itemComponent(
             at: IndexPath(
                 row: 0,
                 section: 0
             )
         )
-
+        
         XCTAssertEqual(
             firstItemComponent.contentMode,
             .fixed(
                 size: CGSize(
-                    width: 500.0,
+                    width: 100.0,
                     height: 100.0
                 )
             )
@@ -166,122 +163,42 @@ internal final class UIListComponentTests: XCTestCase {
         XCTAssertEqual(
             firstItemComponent.view.frame.size,
             CGSize(
-                width: 500.0,
+                width: 100.0,
                 height: 100.0
             )
         )
-
-        let secondItemComponent = listComponent.itemComponent(
+        
+        let secondItemComponent = collectionComponent.itemComponent(
             at: IndexPath(
                 row: 1,
                 section: 0
             )
         )
-
+        
         XCTAssertEqual(
             secondItemComponent.contentMode,
             .automatic(
                 estimatedSize: CGSize(
-                    width: 500.0,
+                    width: 50.0,
                     height: 50.0
                 )
             )
         )
-
+        
         XCTAssertEqual(
             secondItemComponent.view.frame.size,
             CGSize(
-                width: 500.0,
+                width: 50.0,
                 height: expectedLabelSize.height
             )
         )
         
     }
 
-    internal final func testRenderHeaderComponent() {
-
-        let squareComponent = UIItemComponent(
-            contentMode: .fixed(
-                size: CGSize(
-                    width: 100.0,
-                    height: 100.0
-                )
-            ),
-            itemView: UIView()
-        )
-
-        let listComponent = UIListComponent(
-            contentMode: .automatic(
-                estimatedSize: CGSize(
-                    width: 500.0,
-                    height: 50.0
-                )
-            )
-        )
-
-        listComponent.headerComponent = squareComponent
-
-        listComponent.render()
-
-        XCTAssertEqual(
-            listComponent.tableView.tableHeaderView,
-            squareComponent.view
-        )
-
-        XCTAssertEqual(
-            listComponent.headerComponent?.view.frame.size,
-            CGSize(
-                width: 500.0,
-                height: 100.0
-            )
-        )
-
-    }
-
-    internal final func testRenderFooterComponent() {
-
-        let squareComponent = UIItemComponent(
-            contentMode: .fixed(
-                size: CGSize(
-                    width: 100.0,
-                    height: 100.0
-                )
-            ),
-            itemView: UIView()
-        )
-
-        let listComponent = UIListComponent(
-            contentMode: .automatic(
-                estimatedSize: CGSize(
-                    width: 500.0,
-                    height: 50.0
-                )
-            )
-        )
-
-        listComponent.footerComponent = squareComponent
-
-        listComponent.render()
-
-        XCTAssertEqual(
-            listComponent.tableView.tableFooterView,
-            squareComponent.view
-        )
-
-        XCTAssertEqual(
-            listComponent.footerComponent?.view.frame.size,
-            CGSize(
-                width: 500.0,
-                height: 100.0
-            )
-        )
-
-    }
-
     internal final func testKeepStrongReferencesToItemComponentsWhileRendering() {
-
+        
         let colorComponentFactory: (UIColor) -> Component = { color in
-
+            
             let component = UIItemComponent(
                 contentMode: .fixed(
                     size: CGSize(
@@ -291,16 +208,16 @@ internal final class UIListComponentTests: XCTestCase {
                 ),
                 itemView: UIView()
             )
-
+            
             component.view.backgroundColor = color
-
+            
             return component
-
+            
         }
-
+        
         let redComponent = colorComponentFactory(.red)
-
-        let listComponent = UIListComponent(
+        
+        let collectionComponent = UIListComponent(
             contentMode: .automatic(
                 estimatedSize: CGSize(
                     width: 500.0,
@@ -308,50 +225,50 @@ internal final class UIListComponentTests: XCTestCase {
                 )
             )
         )
-
-        listComponent.setItemComponents(
+        
+        collectionComponent.setItemComponents(
             [ redComponent ]
         )
-
-        listComponent.render()
-
+        
+        collectionComponent.render()
+        
         XCTAssertEqual(
-            listComponent.itemComponentCache.count,
+            collectionComponent.itemComponentCache.count,
             1
         )
-
+        
         let firstItemIndexPath = IndexPath(
             item: 0,
             section: 0
         )
-
+        
         XCTAssert(
-            listComponent.itemComponentCache[firstItemIndexPath]
+            collectionComponent.itemComponentCache[firstItemIndexPath]
             === redComponent
         )
-
+        
         let blueComponent = colorComponentFactory(.blue)
-
-        listComponent.setItemComponents(
+        
+        collectionComponent.setItemComponents(
             [ blueComponent ]
         )
-
-        listComponent.render()
-
+        
+        collectionComponent.render()
+        
         XCTAssertEqual(
-            listComponent.itemComponentCache.count,
+            collectionComponent.itemComponentCache.count,
             1
         )
-
+        
         XCTAssert(
-            listComponent.itemComponentCache[firstItemIndexPath]
+            collectionComponent.itemComponentCache[firstItemIndexPath]
             === blueComponent
         )
-
+        
     }
-
+    
     internal final func testGetItemComponentByIndexPath() {
-
+        
         let squareComponent = UIItemComponent(
             contentMode: .fixed(
                 size: CGSize(
@@ -361,8 +278,8 @@ internal final class UIListComponentTests: XCTestCase {
             ),
             itemView: UIView()
         )
-
-        let listComponent = UIListComponent(
+        
+        let collectionComponent = UIListComponent(
             contentMode: .automatic(
                 estimatedSize: CGSize(
                     width: 500.0,
@@ -370,22 +287,22 @@ internal final class UIListComponentTests: XCTestCase {
                 )
             )
         )
-
-        listComponent.setItemComponents(
+        
+        collectionComponent.setItemComponents(
             [ squareComponent ]
         )
-
-        listComponent.render()
-
-        let firstComponent = listComponent.itemComponent(
+        
+        collectionComponent.render()
+        
+        let firstComponent = collectionComponent.itemComponent(
             at: IndexPath(
                 item: 0,
                 section: 0
             )
         )
-
+        
         XCTAssert(firstComponent === squareComponent)
-
+        
     }
-
+    
 }
